43feb6f697f3772267b14b5c759374561d8c9727
DheerajSindeR <160198948+DheerajSindeR@users.noreply.github.com>
2025-09-13 15:21:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app=Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, world !!!'
+    return 'Hello, dheeraj !!!'
 
 if __name__=='__main__':
+
     app.run(host='0.0.0.0', port=5000, debug=False)
